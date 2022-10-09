26e975f2f38ff62757c73a4145797356fce3c110
= <hosnyomar646@gmail.com>
2022-10-09 15:42:48 +0200

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule } from '@angular/core';
+import { AuthGuradService } from './auth-gurad.service';
+AuthGuradService;
+const appRouters: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', canActivate: [AuthGuradService], component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  {
+    path: 'servers',
+    canActivate: [AuthGuradService],
+    component: ServersComponent,
+  },
+  { path: 'servers/:id/edit', component: EditServerComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+
+  { path: '**', redirectTo: '/not-found' },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(appRouters)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/auth-gurad.service.ts
@@ -0,0 +1,31 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+@Injectable()
+export class AuthGuradService implements CanActivate {
+  constructor(private AuthService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.AuthService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+        // return false;
+      }
+    });
+  }
+}
